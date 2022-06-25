5c08aa535d5c3cec7e43ed4be78134f4f1d601ad
Екатерина Захарова <zakharovaekaterina95@mail.ru>
2022-06-25 17:21:31 +0300

    1, 2 task

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,22 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
+
+  result = arr1.lenght === arr2.lenght && arr1.every((item) => arr1[item] === arr2[item]);
+
 
   return result; // boolean
 }
 
+
+
+
 function advancedFilter(arr) {
   let resultArr;
+  
+  resultArr = arr.filter((item) => item >= 0).filter((item) => item % 3 === 0).map((item) => item * 10);
 
-  // Ваш код
+
 
   return resultArr; // array
 }
